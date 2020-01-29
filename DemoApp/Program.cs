f70fe89505e0ad5cc8a2620e82433c0887bfb26f
marian122 <maleksiev12@gmail.com>
2020-01-29 16:41:14 +0200

    Added route class and working with actions for views.

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using SIS.HTTP;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoApp
@@ -8,10 +10,64 @@ namespace DemoApp
     {
         static async Task Main()
         {
-            var httpServer = new HttpServer(80);
+            var routeTable = new List<Route>();
+
+            routeTable.Add(new Route(HttpMethodType.Get, "/", Index));
+            routeTable.Add(new Route(HttpMethodType.Get, "/users/login", Login));
+            routeTable.Add(new Route(HttpMethodType.Post, "/users/login", DoLogin));
+            routeTable.Add(new Route(HttpMethodType.Get, "/contact", Contact));
+            routeTable.Add(new Route(HttpMethodType.Get, "/favicon.ico", FavIcon));
+
+
+
+            var httpServer = new HttpServer(80, routeTable);
             await httpServer.StartAsync();
         }
 
+        private static HttpResponse FavIcon(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
 
+        public static HttpResponse Index(HttpRequest request)
+        {
+            var content = "<h1>this is home page</h1>";
+            byte[] fileContent = Encoding.UTF8.GetBytes(content);
+            var response = new HttpResponse(HttpStatusCode.Ok, fileContent);
+            response.Headers.Add(new Header("Content-Type", "text/html"));
+
+            return response;
+        }
+
+        public static HttpResponse Contact(HttpRequest request)
+        {
+            var content = "<h1>this is contact page</h1>";
+            byte[] fileContent = Encoding.UTF8.GetBytes(content);
+            var response = new HttpResponse(HttpStatusCode.Ok, fileContent);
+            response.Headers.Add(new Header("Content-Type", "text/html"));
+
+            return response;
+        }
+
+        public static HttpResponse Login(HttpRequest request)
+        {
+            var content = "<h1>this is login page</h1>";
+            byte[] fileContent = Encoding.UTF8.GetBytes(content);
+            var response = new HttpResponse(HttpStatusCode.Ok, fileContent);
+            response.Headers.Add(new Header("Content-Type", "text/html"));
+
+            return response;
+        }
+        
+        public static HttpResponse DoLogin(HttpRequest request)
+        {
+            var content = "<h1>this is random page</h1>";
+            byte[] fileContent = Encoding.UTF8.GetBytes(content);
+            var response = new HttpResponse(HttpStatusCode.Ok, fileContent);
+
+            return response;
+        }
     }
+
+    
 }
--- SIS.HTTP/HttpServer.cs
+++ SIS.HTTP/HttpServer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -10,13 +12,16 @@ namespace SIS.HTTP
     {
 
         private readonly TcpListener tcpListener;
+        private readonly IList<Route> routeTable;
 
-        //TODO: actions
-        public HttpServer(int port)
+
+        public HttpServer(int port, IList<Route> routeTable)
         {
             this.tcpListener = new TcpListener(IPAddress.Loopback, port);
-
+            this.routeTable = routeTable;
         }
+
+        public IList<Route> RouteTable { get; }
 
         public async Task ResetAsync()
         {
@@ -51,24 +56,20 @@ namespace SIS.HTTP
                 string requestAsString = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
 
                 var request = new HttpRequest(requestAsString);
+                var route = this.routeTable.FirstOrDefault(x => x.HttpMethod == request.HttpMethod && x.Path == request.Path);
 
-                var content = "<h1>this is random page</h1>";
+                HttpResponse response;
 
-                if (request.Path == "/")
-                {
-                    content = "<h1>this is home page</h1>";
+                if (route == null)
+                {  
+                    response = new HttpResponse(HttpStatusCode.NotFound, new byte[0]);
                 }
-                else if (request.Path == "/users/login")
+                else
                 {
-                    content = "<h1>this is login page</h1>";
+                    response = route.Action(request);
                 }
-
-                byte[] fileContent = Encoding.UTF8.GetBytes(content);
-
-                var response = new HttpResponse(HttpStatusCode.Ok, fileContent);
 
                 response.Headers.Add(new Header("Server", "HomeOfficeServer/1.1"));
-                response.Headers.Add(new Header("Content-Type", "text/html"));
 
                 response.Cookies.Add(new ResponseCookie("sid", Guid.NewGuid().ToString())
                 { HttpOnly = true, MaxAge = 3600 });
@@ -94,66 +95,6 @@ namespace SIS.HTTP
 
             }
 
-
-            //using NetworkStream networkStream = tcpClient.GetStream();
-            //try
-            //{
-            //    byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
-            //    int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
-            //    string requestAsString = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-
-            //    var request = new HttpRequest(requestAsString);
-            //    string newSessionId = null;
-            //    var sessionCookie = request.Cookies.FirstOrDefault(x => x.Name == HttpConstants.SessionIdCookieName);
-            //    if (sessionCookie != null && this.sessions.ContainsKey(sessionCookie.Value))
-            //    {
-            //        request.SessionData = this.sessions[sessionCookie.Value];
-            //    }
-            //    else
-            //    {
-            //        newSessionId = Guid.NewGuid().ToString();
-            //        var dictionary = new Dictionary<string, string>();
-            //        this.sessions.Add(newSessionId, dictionary);
-            //        request.SessionData = dictionary;
-            //    }
-
-            //    Console.WriteLine($"{request.Method} {request.Path}");
-
-            //    var route = this.routeTable.FirstOrDefault(
-            //        x => x.HttpMethod == request.Method && x.Path == request.Path);
-            //    HttpResponse response;
-            //    if (route == null)
-            //    {
-            //        response = new HttpResponse(HttpResponseCode.NotFound, new byte[0]);
-            //    }
-            //    else
-            //    {
-            //        response = route.Action(request);
-            //    }
-
-            //    response.Headers.Add(new Header("Server", "SoftUniServer/1.0"));
-
-            //    if (newSessionId != null)
-            //    {
-            //        response.Cookies.Add(
-            //            new ResponseCookie(HttpConstants.SessionIdCookieName, newSessionId)
-            //            { HttpOnly = true, MaxAge = 30 * 3600, });
-            //    }
-
-            //    byte[] responseBytes = Encoding.UTF8.GetBytes(response.ToString());
-            //    await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-            //    await networkStream.WriteAsync(response.Body, 0, response.Body.Length);
-            //}
-            //catch (Exception ex)
-            //{
-            //    var errorResponse = new HttpResponse(
-            //        HttpResponseCode.InternalServerError,
-            //        Encoding.UTF8.GetBytes(ex.ToString()));
-            //    errorResponse.Headers.Add(new Header("Content-Type", "text/plain"));
-            //    byte[] responseBytes = Encoding.UTF8.GetBytes(errorResponse.ToString());
-            //    await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-            //    await networkStream.WriteAsync(errorResponse.Body, 0, errorResponse.Body.Length);
-            //}
         }
     }
 }
