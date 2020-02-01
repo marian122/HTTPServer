12095e95ce5cb5d91a2c45ef49248b7ff1370b42
marian122 <maleksiev12@gmail.com>
2020-02-01 17:55:47 +0200

    added tests but only one passing, some error occured

--- SIS.MvcFramework.Tests/ViewEngineTests.cs
+++ SIS.MvcFramework.Tests/ViewEngineTests.cs
@@ -10,23 +10,44 @@ namespace SIS.MvcFramework.Tests
         [InlineData("OnlyHtmlView")]
         [InlineData("ForForeachIfView")]
         [InlineData("ViewModelView")]
-        public void GetHtlmTest(string testName)
+        public void TestGetHtml(string testName)
         {
-            var viewModel = new TestViewModel
+            var viewModel = new TestViewModel()
             {
                 Name = "Niki",
                 Year = 2020,
                 Numbers = new List<int> { 1, 10, 100, 1000, 10000 },
             };
 
-            var viewContent = File.ReadAllText($"ViewTest/{testName}.html");
-            var expectedViewContent = File.ReadAllText($"ViewTest/{testName}.Expected.html");
+            var viewContent = File.ReadAllText($"ViewTests/{testName}.html");
+            var expectedResultContent = File.ReadAllText($"ViewTests/{testName}.Expected.html");
 
             IViewEngine viewEngine = new ViewEngine();
             var actualResult = viewEngine.GetHtml(viewContent, viewModel);
 
-            Assert.Equal(expectedViewContent, actualResult);
+            Assert.Equal(expectedResultContent, actualResult);
+        }
 
+        [Fact]
+        public void TestGetHtmlWithTemplateModel()
+        {
+            var viewModel = new List<int> { 1, 2, 3 };
+
+            var viewContent = @"
+@foreach (var num in Model)
+{
+<p>@num</p>
+}";
+            var expectedResultContent = @"
+<p>1</p>
+<p>2</p>
+<p>3</p>
+";
+
+            IViewEngine viewEngine = new ViewEngine();
+            var actualResult = viewEngine.GetHtml(viewContent, viewModel);
+
+            Assert.Equal(expectedResultContent, actualResult);
         }
     }
 }
--- SIS.MvcFramework/IViewEngine.cs
+++ SIS.MvcFramework/IViewEngine.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SIS.MvcFramework
+﻿namespace SIS.MvcFramework
 {
-
     public interface IViewEngine
     {
-        string GetHtml(string template, object model);
-
+        string GetHtml(string templateHtml, object model);
     }
 }
